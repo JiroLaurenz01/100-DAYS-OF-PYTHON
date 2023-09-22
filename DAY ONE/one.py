72d881a22ae7535a3fa43934a5b987d252791bc9
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-09-22 22:15:51 +0800

    DAY ONE

--- DAY ONE/one.py
+++ DAY ONE/one.py
@@ -1,1 +1,4 @@
-print("hello worlds")
+print("Welcome to the Band Name Generator.")
+cityName = input("What's name of the city you grew up in?\n")
+petName = input("What's your pet's name?\n")
+print(f"Your band name could be {cityName} {petName}")
