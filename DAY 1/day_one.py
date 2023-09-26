064afa639639c4a7676527e9f458bc0283e23f4d
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-09-26 17:21:18 +0800

    UPDATE FOR DAY ONE

--- DAY 1/day_one.py
+++ DAY 1/day_one.py
@@ -1,3 +1,31 @@
+# PRINTING
+'''
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+'''
+
+# DEBUGGING PRACTICE
+'''
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+'''
+
+# INPUT FUNCTION
+'''
+print(f'{len(input("What is your name? "))}');
+'''
+
+# VARIABLES
+'''
+a = input("a: ")
+b = input("b: ")
+
+print(f"a: {b}");
+print(f"b: {a}");
+'''
+
+# BAND NAME GENERATOR
 print("Welcome to the Band Name Generator.")
 cityName = input("What's name of the city you grew up in?\n")
 petName = input("What's your pet's name?\n")
